82ec9ef5e5d8761d829792f32de74af5d8610ede
NickSHD <s.kolay999@gmail.com>
2018-03-20 09:53:05 +0300

    new

--- B/main.cpp
+++ B/main.cpp
@@ -2,10 +2,4 @@
 
 int main() {
     map < string, string > a;
-    a["script"] = ".py";
-    a["index"] = ".h";
-    a["main"] = ".cpp";
-    for (auto it : a) {
-        cout << it.first << it.second << endl;
-    }
 }
---
+++ CMakeFiles/CmakeProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CmakeProject.dir/B/main.cpp.o"
+  "CmakeProject.pdb"
+  "CmakeProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CmakeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
